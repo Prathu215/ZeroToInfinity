36e79a169c150d1de06f4ed779082a5f29c1f5de
Prathyusha <prathyusha.system@gmail.com>
2021-02-13 12:24:33 -0600

    feature: Basic To Do app in json

---
+++ Basic-Todo-App-json/src/app/app.component.ts
@@ -0,0 +1,62 @@
+import { Component } from '@angular/core';
+import { ITodoItem } from './interfaces/TodoItem';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Prathyusha To-do list';
+
+  todos: ITodoItem[] = [
+    {
+      lable: "Butter milk",
+      priority: 4,
+      done: true
+    },
+    {
+      lable: "Butter",
+      priority: 3,
+      done: false
+    },
+    {
+      lable: "Milk",
+      priority: 4,
+      done: true
+    },
+    {
+      lable: "Fruits",
+      priority: 2,
+      done: true
+    },
+    {
+      lable: "Paneer",
+      priority: 3,
+      done: true
+    },
+    {
+      lable: "Smoothies",
+      priority: 4,
+      done: false
+    }
+
+  ];
+
+  addList(newtodolable: string) {
+    var newtodo = {
+      lable: newtodolable,
+      priority: 5,
+      done: false
+
+    };
+    console.log("Todo list before push: ",newtodo);
+    this.todos.push(newtodo);
+    console.log("Todo list after push: ",newtodo);
+  }
+  // console.log(newtodo);
+
+  deleteList(todo:ITodoItem){
+    this.todos = this.todos.filter(t => t.lable !== todo.lable);
+  }
+}
---
+++ Basic-Todo-App-json/src/app/interfaces/TodoItem.ts
@@ -0,0 +1,5 @@
+export interface ITodoItem {
+    lable: string;
+    priority: number;
+    done: boolean;
+}
